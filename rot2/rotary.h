b1d6f60e050843a7b3e328015ace01c6f2846265
niels <niels@nielss-air.fritz.box>
2017-05-10 22:22:58 +0200

    initial commit

---
+++ rot2/rotary.h
@@ -0,0 +1,19 @@
+/*
+ * rotary.h
+ *
+ *  Created on: 10 Mar 2017
+ *      Author: niels
+ */
+
+#ifndef ROTARY_H_
+#define ROTARY_H_
+uint8_t read_gray_code_from_encoder(void );
+
+/* set inputs for rotary encoder */
+
+
+
+
+#endif /* ROTARY_H_ */
+
+
